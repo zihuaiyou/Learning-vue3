9ec24b6cb54af50155f459e7bbe0c8483c1426cf
zihuaiyou <zihuaiyou1996@qq.com>
2023-08-06 11:00:21 +0800

    递归组件

--- vite-proj2-layout/src/components/Layout/Menu/index.vue
+++ vite-proj2-layout/src/components/Layout/Menu/index.vue
@@ -1,7 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue';
+import Tree from '../Tree/Tree.vue';
+type TreeList = {
+  name: string,
+  icon?: string,
+  children?: TreeList[] | []
+}
+const treeList = reactive<TreeList[]>(
+  [{
+    name: "no.1",
+    children: [
+      {
+        name: "no.1-1",
+        children: [
+          {
+            name: "no.1-1-1",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "no.2",
+    children: [
+      {
+        name: "no.2-1",
+      },
+    ],
+  },
+  {
+    name: "no.3",
+  },
+  ]
+)
+</script>
 
 <template>
-    <div class="zihyou-menu">menu</div>
+  <div class="zihyou-menu">menu
+    <Tree :data="treeList" />
+  </div>
 </template>
 
 <style scoped lang="scss">
---
+++ vite-proj2-layout/src/components/Layout/Tree/Tree.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+type TreeList = {
+    name: string,
+    icon?: string,
+    children?: TreeList[] | []
+}
+defineProps<{
+    data?: TreeList[] | []
+}>()
+
+const clickItem = (item) => console.log(item);
+
+</script>
+<template>
+    <div v-for="(item, index) in data" style="margin-left:10px">
+        <div :key="index" @click = "clickItem"> {{ item.name }}</div>
+        <Tree v-if="item?.children?.length" :data="item?.children" />
+    </div>
+</template>
+<style scoped></style>
